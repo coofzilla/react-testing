8b8d2fe357e2426391125b32c45382a657fded4b
Jay Hernandez <jeric614@yahoo.com>
2021-09-20 14:22:07 +0900

    configured absolute paths

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { shallow } from "enzyme";
-import App from "../App";
-import CommentBox from "../CommentBox";
+//Absolute paths benefits w/global find and replace (if moving things around)
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 
 //wrapped is a component but w/additional functionality
-it("shows a comment box", () => {
-  //shallow render just component and no children
-  const wrapped = shallow(<App />);
+let wrapped;
 
+beforeEach(() => {
+  //shallow render just component and no children
+  wrapped = shallow(<App />);
+});
+
+it("shows a comment box", () => {
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment list", () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
